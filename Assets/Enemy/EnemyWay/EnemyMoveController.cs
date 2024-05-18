7f69a36b2e735e32af3c633e5ecaa54d24bdf823
W1RDy <ivan.alchin5@mail.ru>
2024-05-18 12:32:39 +0500

    Add enemy movement

--- Assets/Enemy/EnemyWay/EnemyMoveController.cs
+++ Assets/Enemy/EnemyWay/EnemyMoveController.cs
@@ -2,17 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMoveController : MonoBehaviour
+public class EnemyMoveController
 {
-    // Start is called before the first frame update
-    void Start()
+    private EnemyWay _way;
+    private Enemy _enemy;
+
+    private Vector2 _currentDestination;
+
+    public EnemyMoveController(EnemyWay way, Enemy enemy)
     {
-        
+        _way = way;
+        _enemy = enemy;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        MoveOnWay();
+    }
+
+    private bool IsEnemyOnDestination()
+    {
+        return _currentDestination == new Vector2(_enemy.transform.position.x, _enemy.transform.position.y);
+    }
+
+    private void MoveOnWay()
+    {
+        if (IsEnemyOnDestination())
+        {
+            _currentDestination = _way.GetNextPointDestination();
+        }
+        _enemy.Move(_currentDestination);
     }
 }
